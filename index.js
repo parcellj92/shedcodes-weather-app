5b559d4a28324abcbf4750f3b4bd796c729ee8d4
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-07-04 10:38:38 -0600

    Updated axios on .wind

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function displayWeather(response) {
   );
   document.querySelector("#currentDes").innerHTML =
     response.data.weather[0].main;
-  document.querySelector("#wind").innerHTML = response.data.wind[2].speed;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
 }
 
 function searchCity(city) {
