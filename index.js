cae83c8442105cac604f2b78a1b8b7b1439083a4
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-07-04 10:42:29 -0600

    Added .humidity

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ function displayWeather(response) {
   document.querySelector("#currentDes").innerHTML =
     response.data.weather[0].main;
   document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 }
 
 function searchCity(city) {
