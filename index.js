6e69118b00f7b081b2c1db152944534114f15808
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-07-08 16:18:09 -0600

    added another row to temp display js

--- index.js
+++ index.js
@@ -63,6 +63,22 @@ function displayForecast() {
         </div>
       </div>
   `;
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
   });
 
   forecastHTML = forecastHTML + `</div>`;
