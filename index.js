5916973cc0611ec16286f361b160db9a8cd91c03
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-07-08 15:45:30 -0600

    replaced days forecast for html band added javascript for weekly forecast pull

--- index.js
+++ index.js
@@ -40,6 +40,35 @@ let currentDay = document.querySelector("h3");
 currentDay.innerHTML = `${day} ${month} ${date}, ${year} ${hour}:${minute}`;
 //
 
+function displayForecast (){
+    let forecastElement = document.querySelector(#forecast);
+    let fays = ["Thu","Fri","Sat","Sun"];
+
+    let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayWeather(response) {
   let windElement = document.querySelector("#wind");
   let cityElement = document.querySelector("#city");
