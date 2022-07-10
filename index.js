777545dadec332d46b3ea725e673dda8a3318599
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-07-10 13:45:16 -0600

    added formatDay function and days array

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ let currentDay = document.querySelector("h3");
 currentDay.innerHTML = `${day} ${month} ${date}, ${year} ${hour}:${minute}`;
 //
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
@@ -51,15 +59,21 @@ function displayForecast(response) {
       forecastHTML +
       `
       <div class="col-2">
-        <div class="weather-forecast-date">${forecastay.dt}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}˚</span>
-          <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}˚</span>
+          <span class="weather-forecast-temperature-max"> ${
+            forecastDay.temp.max
+          }˚</span>
+          <span class="weather-forecast-temperature-min"> ${
+            forecastDay.temp.min
+          }˚</span>
         </div>
       </div>
   `;
