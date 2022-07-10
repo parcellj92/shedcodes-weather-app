dae32e58a42b97192c6d43b63d040f4f6935737b
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-07-10 13:20:32 -0600

    added apiUrl and key to getForecast function

--- index.js
+++ index.js
@@ -72,6 +72,9 @@ function displayForecast() {
 
 function getForecast(coordinates) {
   console.log(coordinates);
+  let apiKey = "80e7e03978eecacba428a41c3f4a5874";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  console.log(apiUrl);
 }
 
 function displayWeather(response) {
