1ba50ddc8259a28c03346544888b67ce4576336b
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-07-10 13:53:01 -0600

    added index to forecastDay call

--- index.js
+++ index.js
@@ -54,12 +54,13 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay) {
+  forecast.forEach(function (forecastDay, index) {
     forecastHTML =
       forecastHTML +
       `
       <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        ${index}
         <img
           src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
@@ -68,12 +69,12 @@ function displayForecast(response) {
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${
+          <span class="weather-forecast-temperature-max"> ${Math.round(
             forecastDay.temp.max
-          }˚</span>
-          <span class="weather-forecast-temperature-min"> ${
+          )}˚</span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temp.min
-          }˚</span>
+          )}˚</span>
         </div>
       </div>
   `;
