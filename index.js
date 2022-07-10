74e5aa02f3ccbcf858718656ca34737bdcde86aa
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-07-10 13:37:03 -0600

    added getForecast function and added calls for max and min

--- index.js
+++ index.js
@@ -40,26 +40,26 @@ let currentDay = document.querySelector("h3");
 currentDay.innerHTML = `${day} ${month} ${date}, ${year} ${hour}:${minute}`;
 //
 
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${forecastay.dt}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}˚</span>
+          <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}˚</span>
         </div>
       </div>
   `;
@@ -75,6 +75,7 @@ function getForecast(coordinates) {
   let apiKey = "80e7e03978eecacba428a41c3f4a5874";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeather(response) {
@@ -94,7 +95,6 @@ function displayWeather(response) {
 
   getForecast(response.data.coord);
 }
-displayForecast();
 
 function searchCity(city) {
   let apiKey = "80e7e03978eecacba428a41c3f4a5874";
